ce461d1299d0fc7c5a86f6af072377ce7f34a40e
Shadman Jubaer <shadmanjubaer@gmail.com>
2015-03-30 00:47:10 -0500

    Completed Pyramid.java

--- CS106a-Programming-Methodology-Stanford/Assignment2-JAVA/Pyramid.java
+++ CS106a-Programming-Methodology-Stanford/Assignment2-JAVA/Pyramid.java
@@ -14,6 +14,7 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
+// Build a pyramid that is centered and starts at the bottom of the screen.
 public class Pyramid extends GraphicsProgram {
 
 /** Width of each brick in pixels */
@@ -26,7 +27,36 @@ public class Pyramid extends GraphicsProgram {
 	private static final int BRICKS_IN_BASE = 14;
 	
 	public void run() {
-		/* You fill this in. */
+		//Bricks must start at the bottom of the screen
+		//and be centered.'
+		//Find width of window
+		int cx = getWidth()/2;
+		//Find Height of window
+		int cy = getHeight();
+		//Set x position of first brick
+		int brickStart_x = cx - (BRICKS_IN_BASE*BRICK_WIDTH/2);
+		//Set y position of first brick
+		int brickStart_y = cy;
+		//Start at bottom row, and build up
+		int currentRow = BRICKS_IN_BASE;
+		while (currentRow > 0){
+			placeRow(currentRow,brickStart_x,brickStart_y);
+			brickStart_x += BRICK_WIDTH/2;
+			brickStart_y -= BRICK_HEIGHT;
+			currentRow -= 1;
+		}
+	}
+	
+	//Creates a row of blocks given starting coordinates and number of blocks
+	private void placeRow(int bricks2Lay,int x_pos,int y_pos){
+		int rowPosition = 0;
+		int newX = x_pos;
+		while(rowPosition < bricks2Lay){
+			GRect block = new GRect(newX,y_pos,BRICK_WIDTH,BRICK_HEIGHT);
+			add(block);
+			newX+=BRICK_WIDTH;
+			rowPosition+=1;
+		}
 	}
 }
 
